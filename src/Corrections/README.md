3811390fc5cdcc58c5c318a1fe3ee867f19d7021
petermatts <mpeters0514@gmail.com>
2023-03-06 15:47:17 -0500

    Started working on error link logger

---
+++ src/Corrections/README.md
@@ -0,0 +1,3 @@
+### /src/Corrections
+
+This folder will contain all brand individual ccorrections scripts which will be imported to `src/Corrections.py` to run corrections on the data. 
--- src/ErrLinkLogger.py
+++ src/ErrLinkLogger.py
@@ -1,1 +1,38 @@
 # Log all links that produce an error so that potential corrections may be sought out manually
+
+import requests
+import time
+import os
+
+# Traverse Links Directory
+def getLinks():
+    L = []
+
+    # TODO  traverse link directory and call checkLink on each brand \
+    #   alternatively use Docs/AllLinks.txt but Links directory should be better
+    # TODO print percent progress of checking each brand
+    # TODO be sure to sleep for a small amount of time between requests
+
+    # Tests
+    checkLink('https://www.caranddriver.com/mercedes-amg/glc-class/specs', L) # status 404
+    checkLink('https://www.caranddriver.com/lamborghini/hurac%EF%BF%BDn/specs', L) # status 404
+    checkLink('https://www.caranddriver.com/honda/accord/specs', L) # status 200
+    checkLink('https://www.caranddriver.com/honda/civic/specs', L) # status 200
+    checkLink('https://www.caranddriver.com/tesla/cybertruck/specs', L) # status 404
+
+    #? make it a csv instead and put corrected link in next col
+    # file = open('../ErrorLinks.txt', 'w')
+    file = open('../ErrorLinks.csv', 'w')
+    file.writelines(L)
+    file.close()
+
+# Verify if link is good or bad (gives error code)
+def checkLink(url: str, L: list):
+    r = requests.get(url)
+    print(r.status_code)
+
+    if r.status_code != 200:
+        L.append(url + ',' + str(r.status_code) + ',\n')
+
+if __name__ == '__main__':
+    getLinks()
